e61f3540b9184e7eaba4b727826babfd9783ccba
Student <student@RU-017.local>
2015-09-09 15:58:31 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+
+// //tripleFive
+
+// var tripleFive = function(){
+
+
+
+// for(i = 1; i < 4; i ++){
+	
+	
+// 	console.log("Five")
+	
+// 	}
+// }
+// tripleFive()
+// ---------------
+
+//lastLetter
+
+// var lastLetter = function(){
+
+// var hello ='hello'.charAt('hello'.length-1)
+// var goodbye = 'goodbye'.charAt('goodbye'.length-1)		
+// 		console.log(hello)
+// 		console.log(goodbye)
+// }
+
+// lastLetter()
+
+// -----------------------------------
+
+
+//Square//
+
+var square= function(){
+
+	var value = 8     // enter number you wish to square here//
+		
+
+		console.log(Math.pow(value,2));
+
+
+}
+ square()
