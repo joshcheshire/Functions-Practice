25766a88445e4c70fcb23d2ee2b8a08c80915eaf
Student <student@RU-017.local>
2015-09-09 17:17:21 -0600

    completed

--- main.js
+++ main.js
@@ -15,8 +15,17 @@
 // tripleFive()
 // ---------------
 
-//lastLetter
+//lastLetter//
 
+// var lastLetter = function(word){
+
+// 	return word.charAt(word.length-1)
+
+// }
+
+// 	console.log(lastLetter('gerry'))
+
+// --------------------------------------------------
 // var lastLetter = function(){
 
 // var hello ='hello'.charAt('hello'.length-1)
@@ -32,13 +41,159 @@
 
 //Square//
 
-var square= function(){
 
-	var value = 8     // enter number you wish to square here//
+// var square = function(number){
+
+// 		return number * number
+
+
+// }
+// console.log(square(5))
+
+
+
+// ------------------------------------------------
+
+
+// var square= function(number){
+
+// 	var value = 8     // enter number you wish to square here//
 		
 
-		console.log(Math.pow(value,2));
+// 		console.log(Math.pow(value,2));
 
 
-}
- square()
+// }
+//  square()
+
+// ------------------------------------
+
+
+//Negate//
+
+// var negate = function(number){
+
+// 	var value = -4 // enter number to be negated//
+
+// 	console.log( value * -1)
+// 	return (value
+// }
+// -------------------------------
+
+// var negate = function(number) {
+// 	return -number
+
+// }
+// console.log (negate(40))
+
+// negate()
+
+
+//----------------------------------------
+
+// To Array //
+
+// var toArray= function(a, b, c){
+
+
+// var myArray =[a, b, c]
+// return myArray
+
+// }
+
+// console.log(toArray(1, 3, 5))
+
+// //-----------------------------------------
+
+
+// var startsWithA = function(word){
+
+// 	if(word.charAt(0) === 'a'){
+// 		return true
+// 	}
+
+// 	else{
+
+// 		return false
+// 	}
+// }
+
+// console.log(startsWithA('aardvark'))
+// console.log(startsWithA('false'))
+
+// --------------------------------------------
+
+
+//Excite//
+
+
+
+// var excite = function(word){
+
+// 	return word + '!!!'
+
+// }
+
+// console.log(excite('GO'))
+
+// ----------------------------------------
+
+// S U N
+
+
+// var hasSun = function(word){
+
+// 	if(word .includes('sun')){
+
+// 		return true
+// 	}
+
+// 	else{
+
+// 		return false
+// 	}
+
+// }
+
+// console.log(hasSun('asunder'))
+// console.log(hasSun('metal'))
+
+// ---------------------------------------------
+
+// t i n y
+
+// var tiny = function(number){
+
+
+// 	if(number <= 1 && number >=0){
+
+// 		return true
+// 	}
+
+// 	else{
+// 		return false
+// 	}
+// }
+
+// console.log(tiny(-4))
+
+// -------------------------------------------
+
+// g e t   s e c o n d s //  
+
+
+// var getSeconds = function(string){
+
+// 	var split = string.split(':')  
+// 	var mins =Number(split[0]) 
+// 	var seconds=Number(split[1])
+// 	var totalSeconds = (mins * 60) + seconds
+// 	return totalSeconds
+// }
+
+// 	console.log(getSeconds('01:45'))
+// 	console.log(getSeconds('10:30'))
+
+
+
+
